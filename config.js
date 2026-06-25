53069bdf8d40a3530a836c649eba0b2a7b534f7e
Aaronman710 <aarondulong0710@gmail.com>
2026-06-25 17:21:57 +0200

    Update config.js

--- config.js
+++ config.js
@@ -9,15 +9,15 @@ const CONFIG = {
   // L'URL exacte où sera hébergée la page (doit être EXACTEMENT la même
   // dans le dashboard Spotify > Redirect URIs). Pour GitHub Pages, ça
   // ressemble à : https://tonpseudo.github.io/nom-du-repo/
-  REDIRECT_URI: window.location.origin + window.location.pathname,
+  REDIRECT_URI: "https://aaronman710.github.io/Aard.studio/index.html",
 
   // L'identifiant de la playlist Spotify (dans l'URL ou le lien de partage,
   // c'est la suite de lettres/chiffres après "playlist/")
   // Ex: https://open.spotify.com/playlist/37i9dQZF1DXcBWIGoYBM5M -> 37i9dQZF1DXcBWIGoYBM5M
-  PLAYLIST_ID: "COLLE_L_ID_DE_TA_PLAYLIST_ICI",
+  PLAYLIST_ID: "4HMWVkWC3LwxGZvXGccwOp?si=d859da9483d44df8&pt=1fc7c45606f348d007fe9907204cd301",
 
   // Nom affiché en haut de la page
-  PARTY_NAME: "L'anniversaire de Claire 🎉",
+  PARTY_NAME: "L'anniversaire de Aaron et Mailyn 🎉",
 
   // Délai entre deux rafraîchissements automatiques de la playlist (ms)
   POLL_INTERVAL_MS: 8000,
